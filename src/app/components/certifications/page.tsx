0472f654a0ea58d901b499fdb050e447f0016adb
AyeshaMaryam-1 <maryamayesha681@gmail.com>
2024-11-03 01:48:22 +0500

    Add files via upload

---
+++ src/app/components/certifications/page.tsx
@@ -0,0 +1,11 @@
+export default function Certifications(){
+    return(
+        <div className="certifications">
+            <div className="page-text">
+                <h1>COMPANY</h1>
+                <h2>Certifications</h2>
+                <div className="blank"></div>
+            </div>
+        </div>
+    )
+}
